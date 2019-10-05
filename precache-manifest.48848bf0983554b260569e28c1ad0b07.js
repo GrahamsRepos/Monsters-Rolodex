47ee994b0b10f85793d76949496cb8db688b7b14
graham <grahamn@agileworks.co.za>
2019-10-05 08:05:05 +0200

    Updates

---
+++ precache-manifest.48848bf0983554b260569e28c1ad0b07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8267f1c3891a8f0648c1a8a12dc54038",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "7b1c172922c6c47dbbe7",
+    "url": "/Monsters-Rolodex/static/css/main.95330a6a.chunk.css"
+  },
+  {
+    "revision": "abc1ce18aadecde23a06",
+    "url": "/Monsters-Rolodex/static/js/2.e011650a.chunk.js"
+  },
+  {
+    "revision": "7b1c172922c6c47dbbe7",
+    "url": "/Monsters-Rolodex/static/js/main.cc0830b1.chunk.js"
+  },
+  {
+    "revision": "a1fbbb174b32e60ae4fc",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a8038a28.js"
+  }
+]);
